78738a3512497b4f2793f648295826d1adc629ee
AnastasiaPld <nastq_91_pld@abv.bg>
2021-09-22 23:18:36 +0300

    new Changes

--- src/main/java/Bet.java
+++ src/main/java/Bet.java
@@ -4,9 +4,11 @@ public class Bet {
 
     public static void main(String[] args) {
 
-        Calculation calculate = new Calculation();
-        calculate.setFinalScore(6);
-        calculate.setTeam("Manchester");
-        calculate.outcome("Manchester",6);
+        Calculation calculate = new Calculation("Manchester",5);
+
+//        calculate.setFinalScore(6);
+//        calculate.setTeam("Manchester");
+        System.out.println(  calculate.outcome("Manchester",5));
     }
+
 }
--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -1,30 +1,40 @@
 import java.util.Scanner;
 
 public class Calculation {
+    public Calculation(String team, int finalScore) {
+        this.team = team;
+        this.finalScore = finalScore;
+        System.out.println("Outcome of this season play is:" + team + " " + finalScore);
+    }
 
     String team;
     int finalScore;
+//
+//    public void setTeam(String team) {
+//        this.team = team;
+//    }
+//
+//    public void setFinalScore(int finalScore) {
+//        this.finalScore = finalScore;
+//    }
 
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
-    public void setFinalScore(int finalScore) {
-        this.finalScore = finalScore;
-    }
-
-    public void outcome(String guess, int score) {
-
+    public String outcome(String guess, int score) {
+        String prize;
 
         if (score == finalScore && guess == team) {
-            System.out.println("Congratulations you win 100$! Your guess is correct for team and score!");
+            prize = "300$";
+           return prize + " " + String.format("Congratulations you win is %s. Your guess is correct for team and score!", prize);
         } else if (score != finalScore || guess == team) {
-            System.out.println("Congratulations you win 10$! Your guess for winning team is correct!");
-        } else if (finalScore == score) {
-            System.out.println("Congrats your award is 200$. Your guess about the score is correct!");
+            prize = "10$";
+            return prize + " " +  String.format("Congratulations you win is %s. Your guess for winning team is correct!", prize);
+        } else if (finalScore == score || guess != team) {
+            prize = "200$";
+            return prize + " " + String.format("Congrats your win is %s Your guess about the score is correct!", prize);
+
         } else if (finalScore != score && guess != team) ;
+        prize = "0";
         {
-            System.out.println("Your guess is incorrect! No rewards!");
+            return prize +" " +  String.format("Your answer is incorrect and rewards is %s", prize);
 
         }
     }
