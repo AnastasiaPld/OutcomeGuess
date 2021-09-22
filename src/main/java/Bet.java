459d74e2d0adbbb1f3432e10f5cbf36bb355b90a
AnastasiaPld <nastq_91_pld@abv.bg>
2021-09-22 13:25:57 +0300

    Changes

--- src/main/java/Bet.java
+++ src/main/java/Bet.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Bet {
 
     public static void main(String[] args) {
+
         Calculation calculate = new Calculation();
-        calculate.outcome("Carolina Panthers",6);
+        calculate.setFinalScore(6);
+        calculate.setTeam("Manchester");
+        calculate.outcome("Manchester",6);
     }
 }
--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -2,24 +2,34 @@ import java.util.Scanner;
 
 public class Calculation {
 
-        public void outcome(String guess, int score){
-            int finalScore = 6; //score for Carolina Panthers
-            String teamEn = "Carolina Panthers"; //winning team
-            String team2 = "Chicago Bears";//lost the game
+    String team;
+    int finalScore;
 
-            if (score == finalScore && guess ==teamEn){
-                System.out.println("Congratulations you win 100$! Carolina Panthers wins with score 6.");
-            } else if(score!=finalScore || guess ==teamEn){
-                System.out.println("Congratulations you win 10$! Your guess for winning team is correct!");
-            } else if(score!= finalScore && guess==team2){
-                System.out.println("Your guess is not correct! Team Chicago Bears lost.");
-            }else{
-                System.out.println("Your guess is incorrect! No rewards!");
-            }
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setFinalScore(int finalScore) {
+        this.finalScore = finalScore;
+    }
+
+    public void outcome(String guess, int score) {
+
+
+        if (score == finalScore && guess == team) {
+            System.out.println("Congratulations you win 100$! Your guess is correct for team and score!");
+        } else if (score != finalScore || guess == team) {
+            System.out.println("Congratulations you win 10$! Your guess for winning team is correct!");
+        } else if (finalScore == score) {
+            System.out.println("Congrats your award is 200$. Your guess about the score is correct!");
+        } else if (finalScore != score && guess != team) ;
+        {
+            System.out.println("Your guess is incorrect! No rewards!");
+
         }
     }
 
-
+}
 
 
 
